d365722e877b8b418c5b0c8b5f9049257bf5d09d
romarevgreen-commits <romarevgreen@gmail.com>
2026-09-03 15:32:57 -0400

    Add Macon Service Connect Supabase client config

---
+++ macon-service-connect/supabase-config.js
@@ -0,0 +1,3 @@
+const MSC_SUPABASE_URL = 'https://opyzvpsbjqcdfeircica.supabase.co';
+const MSC_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_dZXo7TQxYnqloxuvYJ5hxA_MMNdDOIK';
+window.mscSupabase = window.supabase.createClient(MSC_SUPABASE_URL, MSC_SUPABASE_PUBLISHABLE_KEY);
